22bb654c16cd524e6c31f0326364b5eaa933284f
Eric Burcham <jameseburcham@gmail.com>
2018-06-18 10:26:10 +0530

    Finished method invocation example.

---
+++ Source/Method Invocation.js
@@ -0,0 +1,19 @@
+// Define an object that increments values.
+var incrementer = {
+    // Define the value
+    value: 0,
+
+    // Define an increment function.
+    increment: function(inc) {
+        // 'this' is bound to the containing object.
+        this.value += typeof inc === 'number'
+            ? inc
+            : 1;
+    }
+};
+
+incrementer.increment();
+console.log(incrementer.value); // 1
+
+incrementer.increment(9);
+console.log(incrementer.value); // 10
